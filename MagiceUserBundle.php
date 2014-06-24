025108f6220f454e4073aa40753d9e483256d99c
liverbool <nukboon@gmail.com>
2014-06-24 13:56:14 +0700

    c

--- MagiceUserBundle.php
+++ MagiceUserBundle.php
@@ -2,10 +2,15 @@
 
 namespace Magice\Bundle\UserBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MagiceUserBundle extends Bundle
 {
+    public function build(ContainerBuilder $builder)
+    {
+    }
+
     public function getParent()
     {
         return 'FOSUserBundle';
