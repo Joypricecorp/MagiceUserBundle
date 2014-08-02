b0b7aff900c5649e553e2dbb2c019a7f7b8437ff
liverbool <nukboon@gmail.com>
2014-08-02 19:57:51 +0700

    update dependency package

--- MagiceUserBundle.php
+++ MagiceUserBundle.php
@@ -2,9 +2,7 @@
 
 namespace Magice\Bundle\UserBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\HttpKernel\Kernel;
 
 class MagiceUserBundle extends Bundle
 {
